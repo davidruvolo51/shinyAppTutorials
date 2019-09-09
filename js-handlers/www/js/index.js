a1991bf8c4128c905d2cfdeb2d455e7bcc2f4717
davidruvolo51 <davidruvolo51@gmail.com>
2019-09-09 15:00:34 +0200

    new demo

---
+++ js-handlers/www/js/index.js
@@ -0,0 +1,120 @@
+
+
+// css focused module
+var styles = (function(){
+
+    // add css to element
+    var addCSS = function (elem, css) {
+        var elem = document.querySelector(elem);
+        elem.classList.add(css);
+    }
+
+    // remove css from element
+    var removeCSS = function (elem, css) {
+        var elem = document.querySelector(elem);
+        elem.classList.remove(css);
+    }
+
+    // find current theme of an element
+    var findCSS = function (elem) {
+        var target = document.querySelector(elem);
+        var css = Array.from(target.classList)[0];
+        if (typeof css === "undefined" || css === null) {
+            return "light-theme";
+        } else {
+            return css;
+        }
+    }
+
+    // export
+    return {
+        addCSS    : addCSS,
+        removeCSS : removeCSS,
+        findCSS   : findCSS
+    }
+
+})();
+
+
+// file focused modules
+var file = (function(){
+
+    // save option to local storage
+    var save = function (value) {
+        localStorage.setItem("theme", value);
+    }
+
+    // load option from local storage
+    var load = function () {
+        return localStorage.getItem("theme");
+    }
+
+    // set starting theme based on local storage
+    var setDefault = function (elem) {
+        var defaultTheme = load();
+        if (typeof defaultTheme === "undefined" || defaultTheme === null) {
+            styles.addCSS(elem, "light-theme");
+        } else {
+            styles.addCSS(elem, defaultTheme);
+        }
+    }
+
+    // export
+    return {
+        save       : save,
+        load       : load,
+        setDefault : setDefault
+    }
+
+})();
+
+// text based modules
+var text = (function () {
+
+    // set inner html by id
+    var setInnerHtmlById = function (id, string) {
+        var elem = document.getElementById(id);
+        elem.innerHTML = string
+    }
+
+    // export functions
+    return {
+        setInnerHtmlById : setInnerHtmlById
+    }
+
+})();
+
+// create handler that toggles the theme and saves to local storage
+Shiny.addCustomMessageHandler("toggleTheme", function (value) {
+    
+    // find theme
+    var currentTheme = styles.findCSS(value);
+    var newTheme = '';
+
+    // run logic
+    if (currentTheme === "dark-theme") {
+        styles.removeCSS(value, "dark-theme");
+        styles.addCSS(value, "light-theme");
+        newTheme = "light-theme";
+    } else {
+        styles.removeCSS(value, "light-theme");
+        styles.addCSS(value, "dark-theme");
+        newTheme = "dark-theme";
+    }
+
+    // save theme and update output element
+    text.setInnerHtmlById("themeStatus", newTheme);
+    file.save(newTheme);
+
+});
+
+// create a handler that sets the default theme based on value saved in local storage
+Shiny.addCustomMessageHandler("setDefaultTheme", function (value) {
+
+    // run our set default function - the value is the name of the element we want to apply our style to
+    file.setDefault(value);
+
+    // find the name of the theme applied and update text
+    var currentTheme = styles.findCSS(value);
+    text.setInnerHtmlById("themeStatus", currentTheme);
+});
