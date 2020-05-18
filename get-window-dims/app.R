7169423c16b13e593a327368417cd58705780520
davidruvolo51 <davidruvolo51@gmail.com>
2020-05-18 16:05:34 +0200

    init new tutorial

---
+++ get-window-dims/app.R
@@ -0,0 +1,28 @@
+#'////////////////////////////////////////////////////////////////////////////
+#' FILE: app.R
+#' AUTHOR: David Ruvolo
+#' CREATED: 2020-05-18
+#' MODIFIED: 2020-05-18
+#' PURPOSE: get window dimensions app
+#' STATUS: in.progress
+#' PACKAGES: shiny
+#' COMMENTS: NA
+#'////////////////////////////////////////////////////////////////////////////
+
+# pkgs
+suppressPackageStartupMessages(library(shiny))
+
+# ui
+ui <- tagList(
+    
+)
+
+
+# server 
+server <- function(input, output, session) {
+
+}
+
+
+# app
+shinyApp(ui, server)
