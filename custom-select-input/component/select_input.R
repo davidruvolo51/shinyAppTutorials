129c1a2c3fbc2f0c50c9397e5e0c1ad1933db9d8
davidruvolo51 <davidruvolo51@gmail.com>
2020-07-03 17:20:24 +0200

    added initial keyboard support

--- custom-select-input/component/select_input.R
+++ custom-select-input/component/select_input.R
@@ -23,21 +23,33 @@ helpers <- list()
 #' Define a function that returns component toggle. This button, when clicked,
 #' will open and close the menu (i.e., list of options)
 #' @param inputId the inputId passed down from the parent function
+#' @param labelId ID of the component's input label
 #' @importFrom rheroicons solid$chevron_down
-helpers$list_toggle <- function(inputId) {
-    ns <- NS(inputId)
+helpers$list_toggle <- function(inputId, labelId) {
+    buttonId <- paste0(inputId, "__", "toggle")
+    buttonLabelId <- paste0(buttonId, "_label")
     tags$button(
-        id = ns("toggle"),
+        id = buttonId,
         class = "select-list-toggle",
         `data-group` = inputId,
+        `aria-haspopup` = "listbox",
         `aria-expanded` = "false",
-        tags$span(class = "toggle-text", "Select an item"),
+        `aria-labelledby` = paste0(labelId, " ", buttonLabelId),
+
+        # text element for current selected item
+        tags$span(
+            id = buttonLabelId,
+            class = "toggle-text"
+        ),
+
+        # icon
         rheroicons::solid$chevron_down(
             aria_hidden = TRUE,
             class = "toggle-icon"
         )
     )
 }
+
 
 
 #' ~ 1b ~
@@ -52,19 +64,28 @@ helpers$list_toggle <- function(inputId) {
 #' @param value a value for the input item (if null, will be `option`)
 #' @importFrom rheroicons outline$check_circle
 helpers$input_list_item <- function(inputId, option, value) {
-    ns <- NS(value)
+    forId <- paste0(inputId, "__", option)
+
+    # generate html
     tags$li(
-        class = "select-input-list-item",
-        tags$button(
-            id = ns("input-option"),
-            class = "input-button",
-            `data-value` = value,
-            `data-group` = inputId,
-            rheroicons::solid$check_circle(
-                aria_hidden = TRUE,
-                class = "input-icon"
-            ),
-            tags$span(class = "input-text", option)
+        id = forId,
+        class = "select-input-list-option",
+        role = "option",
+        `data-value` = value,
+        `data-group` = inputId,
+        `aria-labelledby` = forId,
+        tabIndex = "0",
+
+        # selected icon
+        rheroicons::solid$check_circle(
+            aria_hidden = TRUE,
+            class = "input-icon"
+        ),
+        # label
+        tags$span(
+            id = paste0(forId, "-input-label"),
+            class = "input-text",
+            option
         )
     )
 }
@@ -75,14 +96,19 @@ helpers$input_list_item <- function(inputId, option, value) {
 #' This function will generate the list items based on the number of options
 #' indicated in the parent function.
 #' @param inputId the inputId passed down from the parent function
+#' @param labelId Id of the component (generated internally)
 #' @param data a list object containing the options and values.
-helpers$input_list <- function(inputId, data) {
+helpers$input_list <- function(inputId, labelId, data) {
 
-    # generate markup for parent (<ol>) element
-    parent <- tags$ol(
+    # generate markup for parent element
+    parent <- tags$ul(
+        id = paste0(inputId, "__input_list"),
         class = "select-input-list",
         `data-group` = inputId,
-        `aria-hidden` = "false"
+        `aria-hidden` = "false",
+        `aria-labelledby` = labelId,
+        role = "listbox",
+        tabIndex = "-1"
     )
 
     # generate markup for child (<li>) elements
@@ -94,15 +120,8 @@ helpers$input_list <- function(inputId, data) {
         )
     })
 
-    # bind to parent and add blank option
-    parent$children <- tagList(
-        helpers$input_list_item(
-            inputId = inputId,
-            option = "Select an item",
-            value = "NA"
-        ),
-        children
-    )
+    # bind to parent (you can add blank option here)
+    parent$children <- children
 
     # return
     return(parent)
@@ -117,10 +136,11 @@ helpers$input_list <- function(inputId, data) {
 #' component.
 #' @param inputId a unique ID for the input element
 #' @param title a title that describes the select input component
+#' @param label a label that describes what to do
 #' @param options an array used to generate input options
 #' @param values an array of values to pass on to each inputs (gets `options`
 #'              if argument is NULL)
-select_input <- function(inputId, title, options, values = NULL) {
+select_input <- function(inputId, title, label = NULL, options, values = NULL) {
 
     # validate
     stopifnot(is.character(inputId))
@@ -131,15 +151,51 @@ select_input <- function(inputId, title, options, values = NULL) {
     data <- list(options = options, values = options)
     if (!is.null(values)) data$values <- values
 
+    # set ID of text elements (for aria attributes)
+    titleId <- paste0(inputId, "__title")
+    labelId <- paste0(inputId, "__label")
+
+    # set default label
+    if (!is.null(label)) {
+        stopifnot(is.character(label))
+        lab <- label
+    } else {
+        lab <- "Select an option"
+    }
+
     # build component
     el <- tags$fieldset(
         id = inputId,
         class = "select-input-group hidden",
         `data-group` = inputId,
         `data-value` = "NULL",
-        tags$legend(class = "select-input-title", title),
-        helpers$list_toggle(inputId = inputId),
-        helpers$input_list(inputId = inputId, data = data)
+
+        # define title for the input group
+        tags$legend(
+            id = titleId,
+            class = "select-input-title",
+            title
+        ),
+
+        # define label for menu
+        tags$span(
+            id = labelId,
+            class = "select-input-label",
+            lab
+        ),
+
+        # generate menu toggle
+        helpers$list_toggle(
+            inputId = inputId,
+            labelId = labelId
+        ),
+
+        # generate options list
+        helpers$input_list(
+            inputId = inputId,
+            data = data,
+            labelId = labelId
+        )
     )
 
     # return
