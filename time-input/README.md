182c82f599f83896ee32f84b8a41e5ac7e32fcb2
davidruvolo51 <davidruvolo51@gmail.com>
2020-08-01 16:41:38 +0200

    restructured time input example

--- time-input/README.md
+++ time-input/README.md
@@ -1,5 +1,5 @@
 # Time Input
 
-A time input element does not exist for shiny applications. There are a number of packages and widgets available; however, we can easily create one using the browser native `type="time"`. This shiny application demonstrates how to structure the input and register it with shiny for use in your server code. 
+A time input element does not exist for shiny applications. There are a number of packages and widgets available; however, we can easily create one using `type="time"`. This shiny application demonstrates how to structure the input and register it with shiny for use in your server code. 
 
-A longer tutorial is underway. Until then, you can clone this repo and run the application locally. 
+For more information, see the longer post: [Creating a custom time input](https://davidruvolo51.github.io/shinytutorials/tutorials/time-input/)
--- time-input/app.R
+++ time-input/app.R
@@ -2,7 +2,7 @@
 #' FILE: app.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2019-11-17
-#' MODIFIED: 2020-07-31
+#' MODIFIED: 2020-08-01
 #' PURPOSE: single file app for time-input shiny application
 #' STATUS: in.progress
 #' PACKAGES: shiny
@@ -17,18 +17,42 @@ source("time_input.R")
 
 # app
 ui <- tagList(
-    tags$h1("Time Input Example"),
-    tags$form(
-        time_input(
-            inputId = "take_away",
-            label = "Select time you would like to pick up your order",
-            caption = "We are open everyday from 11:00 to 18:00",
-            value = "12:00",
-            min = "11:00",
-            max = "18:00"
+    tags$head(
+        tags$link(rel = "stylesheet", href = "time_input.css"),
+        tags$style(
+            "html, body {
+                font-family: Helvetica;
+                font-size: 16pt;
+            }",
+            "main {
+                width: 90%;
+                max-width: 912px;
+                margin: 0 auto;
+                padding: 32px;
+            }",
+            "#timeOutput {
+                background-color: #353535;
+                color: #f1f1f1;
+                box-sizing: content-box;
+                padding: 12px;
+            }"
         ),
-        tags$h2("Time Selected"),
-        verbatimTextOutput("timeOutput")
+        tags$title("shinyAppTutorials | Time Input Example")
+    ),
+    tags$main(
+        tags$h1("Time Input Example"),
+        tags$form(
+            time_input(
+                inputId = "take_away",
+                label = "What time would you like to pick up your order?",
+                caption = "We are open everyday from 11:00 to 18:00",
+                value = "12:00",
+                min = "11:00",
+                max = "18:00"
+            ),
+            tags$h2("Time Selected"),
+            verbatimTextOutput("timeOutput")
+        )
     ),
     tags$script(src = "time_input.js")
 )
