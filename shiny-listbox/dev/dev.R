693ee0ae15437bfdd7b7027b34d31b98e94e707d
davidruvolo51 <davidruvolo51@gmail.com>
2021-02-16 20:49:44 +0100

    init subdir management

---
+++ shiny-listbox/dev/dev.R
@@ -0,0 +1,16 @@
+#'////////////////////////////////////////////////////////////////////////////
+#' FILE: dev.R
+#' AUTHOR: David Ruvolo
+#' CREATED: 2021-02-16
+#' MODIFIED: 2021-02-16
+#' PURPOSE: subdir management
+#' STATUS: ongoing
+#' PACKAGES: see below
+#' COMMENTS: NA
+#'////////////////////////////////////////////////////////////////////////////
+
+#' pkgs
+install.packages("shiny")
+
+# use remotes until this package is on cran
+remotes::install_github("davidruvolo51/rheroicons@*release")
